f1e30fd21749dd1ba9283f996ffdf3b6644b5815
Andrew <asutherlandavis@gmail.co.uk>
2018-06-18 22:01:59 +0100

    Matthew now creates potions to send to others

---
+++ Kanu Kraze/Assets/PotionScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PotionScript : MonoBehaviour
+{
+    public PlayerScript Player { get; internal set; }
+    public int HealthBenefits;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Player) return;
+
+        StartCoroutine(Transition());
+    }
+
+    private IEnumerator Transition()
+    {
+        yield return 0;
+
+        float transitionDuration = .75f;
+
+        var t = 0.0f;
+        var startingPos = transform.position;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * (Time.timeScale / transitionDuration);
+
+            var destination = new Vector3(Player.transform.position.x, Player.transform.position.y);
+
+            transform.position = Vector3.Lerp(startingPos, destination, t);
+            yield return 0;
+        }
+        Player.HealthGained(HealthBenefits);
+        Destroy(gameObject);
+    }
+}
--- Kanu Kraze/Assets/Scripts/MatthewScript.cs
+++ Kanu Kraze/Assets/Scripts/MatthewScript.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MatthewScript : MonoBehaviour
@@ -7,6 +9,9 @@ public class MatthewScript : MonoBehaviour
     private int _potionsCollected = 0;
     private float _regenerateCount = 0;
     public Text PotionText;
+
+    public Transform Potion;
+    public Sprite[] PotionImages;
 
     // Use this for initialization
     void Start()
@@ -18,12 +23,47 @@ public class MatthewScript : MonoBehaviour
     void Update()
     {
         if (!PlayerScript.Alive) return;
+
         _regenerateCount += Time.deltaTime;
         if (_regenerateCount >= 5)
         {
             _regenerateCount = 0;
             PlayerScript.HealthGained(3);
         }
+
+        if (!PlayerScript.Active) return;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            StartCoroutine(SendPotions(1));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            StartCoroutine(SendPotions(2));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            StartCoroutine(SendPotions(3));
+        }
+    }
+
+    public IEnumerator SendPotions(int quantity)
+    {
+        if (quantity <= _potionsCollected)
+        {
+            var players = GameObject.Find("GameController").GetComponent<GameControllerScript>().Players.Where(p => p.Alive && !p.Complete && p.name != "Matthew");
+            foreach (var item in players)
+            {
+                var obj = Instantiate(Potion, transform.position + new Vector3(0, 2, -1), Quaternion.identity, null);
+                obj.GetComponent<PotionScript>().Player = item;
+                _potionsCollected -= quantity;
+                PotionText.text = _potionsCollected.ToString();
+                obj.GetComponent<PotionScript>().HealthBenefits = 12 * quantity;
+                var random = Random.Range(0, PotionImages.Length);
+                obj.GetComponent<SpriteRenderer>().sprite = PotionImages[random];
+            }
+        }
+        yield return 0;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
